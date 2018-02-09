fc6273a5b420c983d67f9c4517c6089f5590b4eb
Vidyarsk <vidyarangaswamy95@gmail.com>
2018-02-09 16:59:27 +0530

    sir Implemented

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
-function helloVidya()
-{
-    alert("Hello Vidya? Why??");
-        
-}
+const app = angular.module("myApp" , []);
+
+
+
+app.controller("myCtr" , function($scope , $http){
+    $http.get("https://api.github.com/users").then(function(item){
+        $scope.gituser = item.data;
+    });
+
+
+    // $scope.removeItem = function(user){
+    //     var removedItem =  $scope.gituser.indexOf(user);
+    //     $scope.gituser.splice(removedItem , 1)
+
+    // }
+
+ $scope.removeItem = function(x){
+      var removedata = $scope.gituser.indexOf(x);
+      $scope.gituser.splice(removedata , 1)
+    }
+
+});
